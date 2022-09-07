ec8a61abf46f5b484b30eaee0a331b7ccf70ce0e
Ahmed Qureshi <51003720@kdahit.com>
2022-09-07 14:47:56 +0530

    Final Commit

--- bulk_sms_sender_from_database/postgress_config.py
+++ bulk_sms_sender_from_database/postgress_config.py
@@ -1,30 +1,31 @@
 import psycopg2
 import datetime
 
-db_name =  'feedback_database'
-username =  'postgres'
-password = 'ahmed'
-host = '172.20.200.40'
+db_name = "feedback_database"
+username = "postgres"
+password = "ahmed"
+host = "172.20.100.81"
 port = 5432
 
 
 class PostgressDB:
-
     def __init__(self):
         # Connect to your postgres DB
-        self.connenction = psycopg2.connect(host=host,dbname=db_name,user=username,password=password,port=port)
+        self.connenction = psycopg2.connect(
+            host=host, dbname=db_name, user=username, password=password, port=port
+        )
         # Open a cursor to perform database operations
         self.cursor = self.connenction.cursor()
-    
+
     def connection_close(self):
         if self.cursor:
             self.cursor.close()
-        
+
         if self.connenction:
             self.connenction.close()
-    
-    def send_patient_data_to_postgres_database(self,patient_details):
-        send_patient_data_to_database_qurey = ('''
+
+    def send_patient_data_to_postgres_database(self, patient_details):
+        send_patient_data_to_database_qurey = """
         
         
             INSERT INTO "feedback_app_sms_sent_for_feedback"
@@ -32,34 +33,37 @@ class PostgressDB:
             VALUES
             (%s, %s, %s, %s,%s, %s, %s,%s,%s,%s )
 
-        ''')
-        
-        for i in list(patient_details.keys()):
-            date_added = datetime.datetime.now()
-            self.cursor.execute(send_patient_data_to_database_qurey,[   patient_details[i]["patient_id"],
-                                                                        patient_details[i]["encounter_id"],
-                                                                        patient_details[i]["patient_name"],
-                                                                        patient_details[i]["phone_number"],
-                                                                        patient_details[i]["gender"],
-                                                                        patient_details[i]["bed_number"],
-                                                                        patient_details[i]["location"],
-                                                                        patient_details[i]["admission_datetime"],
-                                                                        patient_details[i]["discharge_datetime"],
-                                                                        date_added,
-                                                                        ])
-            self.connenction.commit()                    
-        # data = self.cursor.fetchall()
-                            
-        #return data
+        """
 
+        for i in list(patient_details.keys()):
+            date_added = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            self.cursor.execute(
+                send_patient_data_to_database_qurey,
+                [
+                    patient_details[i]["patient_id"],
+                    patient_details[i]["encounter_id"],
+                    patient_details[i]["patient_name"],
+                    patient_details[i]["phone_number"],
+                    patient_details[i]["gender"],
+                    patient_details[i]["bed_number"],
+                    patient_details[i]["location"],
+                    patient_details[i]["admission_datetime"],
+                    patient_details[i]["discharge_datetime"],
+                    date_added,
+                ],
+            )
+            self.connenction.commit()
+        # data = self.cursor.fetchall()
+
+        # return data
 
     def delete_patient_above_24hr(self):
-        delete_patient_above_24hr_query = ('''
+        delete_patient_above_24hr_query = """
         
         DELETE FROM "feedback_app_sms_sent_for_feedback"
         WHERE "sms_sent_date" < NOW() - INTERVAL '1 DAY'
      
-        ''')
+        """
 
         self.cursor.execute(delete_patient_above_24hr_query)
         self.connenction.commit()
--- bulk_sms_sender_from_database/send_feedback_sms_main.py
+++ bulk_sms_sender_from_database/send_feedback_sms_main.py
@@ -6,32 +6,31 @@ from postgress_config import PostgressDB
 
 # This Script has three different part and functions
 
-#Part 1
+# Part 1
 
-#Retrieve Data from the database and filter the phone number as per requirements
+# Retrieve Data from the database and filter the phone number as per requirements
 pva = PatientsValueAssignment()
 
 patients_list = pva.get_filtered_patients_values()
 
-#---------------------------------------------------------------------------------------------------------------------------------------#
+# ---------------------------------------------------------------------------------------------------------------------------------------#
 
 # Part 2
 
 # Extract Phone number to send SMS in bulk
 extracted_phone_number = []
 for i in list(patients_list.keys()):
-#     print(patients_list[i]['phone_number'])
-    extracted_phone_number.append(patients_list[i]['phone_number'])
+    #     print(patients_list[i]['phone_number'])
+    extracted_phone_number.append(patients_list[i]["phone_number"])
 
 
+# Temproary "Remove in production"
+extracted_phone_number1 = ["8767861216", "8080513540","9820580481"]  # "9004579961"
 
-#Temproary "Remove in production"
-extracted_phone_number1 = ["8767861216","9004579961","8080513540"]
+# Send Bulk SMS
+SendSms(number=",".join(extracted_phone_number1))
 
-#Send Bulk SMS
-SendSms(number=','.join(extracted_phone_number1))
-
-#---------------------------------------------------------------------------------------------------------------------------------------#
+# ---------------------------------------------------------------------------------------------------------------------------------------#
 
 # Part 3 Save the data in a temporary table so that it can be viewed later
 post_db = PostgressDB()
